5fa4f0917653d03cfe8de76dc242bd0b047de4eb
Juan <malachias322@gmail.com>
2023-09-30 18:27:24 -0300

    Criar Front do BlogPessoal

---
+++ src/paginas/home/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+
+function home(){
+    return (
+        <>
+            <h1 >home</h1>
+           
+        </>
+    );
+}
+
+export default home;
